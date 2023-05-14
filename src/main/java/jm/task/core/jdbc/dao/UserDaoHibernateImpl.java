45b45e57b9818a4e95b4e92938f8cf97519be5d4
CCARNIFEXX <112640748+CCARNIFEXX@users.noreply.github.com>
2023-05-14 23:04:12 +0300

    1TaskPPKata

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,83 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final UserDaoJDBCImpl userDaoJDBC;
     public UserDaoHibernateImpl() {
-
+        try {
+            this.userDaoJDBC = new UserDaoJDBCImpl();
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     @Override
     public void createUsersTable() {
-
+        userDaoJDBC.createUsersTable();
     }
 
     @Override
     public void dropUsersTable() {
-
+        userDaoJDBC.dropUsersTable();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            User user = new User();
+            user.setName(name);
+            user.setLastName(lastName);
+            user.setAge(age);
+            session.save(user);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> resultList;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<User> cq = cb.createQuery(User.class);
+            Root<User> rootEntry = cq.from(User.class);
+            CriteriaQuery<User> all = cq.select(rootEntry);
+            TypedQuery<User> allQuery = session.createQuery(all);
+            resultList = allQuery.getResultList();
+            session.getTransaction().commit();
+        }
+        return resultList;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createQuery("delete from User").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,14 @@
 package jm.task.core.jdbc.util;
-
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
@@ -10,16 +16,42 @@ public class Util {
 
     public static Connection getMySQLConnection() throws SQLException,
             ClassNotFoundException {
-        String hostName = "localhost";
-
-        String dbName = "test";
-        String userName = "test";
-        String password = "test";
-
-       Class.forName("com.mysql.jdbc.Driver");
-
-       String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
-
-       return DriverManager.getConnection(connectionURL, userName, password);
+        Properties properties = getDBProperties();
+        String userName = properties.getProperty("hibernate.connection.username");
+        String password = properties.getProperty("hibernate.connection.password");
+        Class.forName(properties.getProperty("hibernate.connection.driver_class"));
+        String connectionURL = properties.getProperty("hibernate.connection.url");
+        return DriverManager.getConnection(connectionURL, userName, password);
     }
+
+    private static SessionFactory sessionFactory = buildSessionFactory();
+
+
+    private static Properties getDBProperties() {
+        Properties properties = new Properties();
+        String appConfigPath = new File("src/main/resources/mySql.properties").getAbsolutePath();
+        try {
+            properties.load(new FileInputStream(appConfigPath));
+        } catch (IOException ignored) {}
+        return properties;
+    }
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            if (sessionFactory == null) {
+                sessionFactory = new Configuration()
+                        .addProperties(getDBProperties())
+                        .addAnnotatedClass(User.class)
+                        .buildSessionFactory();
+            }
+            return sessionFactory;
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+
 }
