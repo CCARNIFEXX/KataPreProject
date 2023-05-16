11bdf72103108495f520a5f268846953915f9075
CCARNIFEXX <112640748+CCARNIFEXX@users.noreply.github.com>
2023-05-16 21:40:52 +0300

    3TaskPPKata

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,14 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+ 
+    @Bean(name="hello world")
+    public HelloWorld getHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
+}
